cc4b7718cca56df3c9b3a03df114aff9bb3f2e36
Mikhail <mikhail.gubin@outlook.com>
2018-04-03 14:00:08 +0300

    урок 3 тяжелое задание

--- Lesson-3/heavy/js/script.js
+++ Lesson-3/heavy/js/script.js
@@ -1,7 +1,17 @@
-let week = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
-let stylesBold = [ style = 'bold'];
+let str = 'урок-3-был слишком легким';
+let arr = [20, 33, 1, 'Человек', 2, 3];
 
-for (var i = 0; i < week.length; i++) {
-	console.log(week[i]);
+let newStr = str.replace(/-/g, ' ');
+newStr = newStr.replace(/у/g, 'У');
 
-}
+console.log(newStr);
+
+newStr = newStr.replace(/ слишком/g, '');
+
+newStr = newStr.slice(0,-2);
+
+newStr = newStr.replace(/был легк/g, 'было легко');
+
+alert(newStr);
+
+
