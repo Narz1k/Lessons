57951af858b08b90c84b2c4ae573c157a330b456
Mikhail <mikhail.gubin@outlook.com>
2018-04-03 20:45:25 +0300

    урок 3 тяжелое задание

--- Lesson-3/heavy/js/script.js
+++ Lesson-3/heavy/js/script.js
@@ -1,17 +1,28 @@
 let str = 'урок-3-был слишком легким';
+
+let newStr = str.replace(/-/g, ' '); // заменяем знак - на пробел
+newStr = newStr.replace(/у/g, 'У'); // заменяем у на У
+
+console.log(newStr); // выводим в консоль
+
+newStr = newStr.replace(/ слишком/g, ''); // удаляем слово слишком
+
+newStr = newStr.slice(0,-2); // убираем из слова легким последние 2 буквы
+
+newStr = newStr.replace(/был легк/g, 'было легко'); // заменяем "был легк" на "было легко"
+
+alert(newStr); // выводим результат на модальное окно
+
+
 let arr = [20, 33, 1, 'Человек', 2, 3];
 
-let newStr = str.replace(/-/g, ' ');
-newStr = newStr.replace(/у/g, 'У');
+arr.splice(3, 1); // удаляем с 3 элемента массива 1 элемен "Человек"
 
-console.log(newStr);
-
-newStr = newStr.replace(/ слишком/g, '');
-
-newStr = newStr.slice(0,-2);
-
-newStr = newStr.replace(/был легк/g, 'было легко');
-
-alert(newStr);
+let result = arr.reduce(function(sum, current) { // Складываем элементы массива
+	return sum + current;
+}, 0);
 
 
+Math.sqrt(result); // Получаем квадратный корень суммы чисел массива arr
+
+console.log(result); // Выводим в консоль результат
